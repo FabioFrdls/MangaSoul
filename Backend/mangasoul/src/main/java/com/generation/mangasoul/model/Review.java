51e1888a06ea94fd70cef6fcf225f755ffce0761
pio918 <bencivengapio18@gmail.com>
2025-09-03 16:07:44 +0200

    class review defined

--- Backend/mangasoul/src/main/java/com/generation/mangasoul/model/Review.java
+++ Backend/mangasoul/src/main/java/com/generation/mangasoul/model/Review.java
@@ -1,5 +1,83 @@
 package com.generation.mangasoul.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
 public class Review {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotNull(message = "Score cannot be blank")
+	private double score;
+
+	private String text;
+
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@ManyToOne
+	@JoinColumn(name = "manga_id")
+	private Manga manga;
+
+	public Review() {
+	}
+
+	public Review(long id, double score, String text, User user, Manga manga) {
+
+		this.id = id;
+		this.score = score;
+		this.text = text;
+		this.user = user;
+		this.manga = manga;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public double getScore() {
+		return score;
+	}
+
+	public void setScore(double score) {
+		this.score = score;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Manga getManga() {
+		return manga;
+	}
+
+	public void setManga(Manga manga) {
+		this.manga = manga;
+	}
 
 }
