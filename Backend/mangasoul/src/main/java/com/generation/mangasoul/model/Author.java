3a97391aa8e5394b1b989127599801c7092fb190
Alessio <samir529800@gmail.com>
2025-09-03 16:24:38 +0200

    Model Autore + Librareria
    Modello di Autore e libreria(incompleta)

--- Backend/mangasoul/src/main/java/com/generation/mangasoul/model/Author.java
+++ Backend/mangasoul/src/main/java/com/generation/mangasoul/model/Author.java
@@ -1,5 +1,66 @@
 package com.generation.mangasoul.model;
 
-public class Author {
+import java.time.LocalDate;
 
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+public class Author {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	
+	@NotBlank(message = "L'autore deve avere nome e cognome ")
+	private String full_name;
+	
+	@NotNull(message = "L'autore deve avere una data di nascita")
+	private LocalDate birthdate;
+
+	
+	public Author(long id, String full_name, LocalDate birthdate){
+		this.id = id;
+		this.full_name = full_name;
+		this.birthdate = birthdate;
+	}
+	
+	public Author() {}
+	
+	public long getId() {
+		return id;
+	}
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	public String getFull_name() {
+		return full_name;
+	}
+
+
+	public void setFull_name(String full_name) {
+		this.full_name = full_name;
+	}
+
+
+	public LocalDate getBirthdate() {
+		return birthdate;
+	}
+
+
+	public void setBirthdate(LocalDate birthdate) {
+		this.birthdate = birthdate;
+	} 
+	
+	
 }
--- Backend/mangasoul/src/main/java/com/generation/mangasoul/repository/AuthorRepository.java
+++ Backend/mangasoul/src/main/java/com/generation/mangasoul/repository/AuthorRepository.java
@@ -1,5 +1,12 @@
 package com.generation.mangasoul.repository;
 
-public interface AuthorRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.generation.mangasoul.model.Author;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Integer> {
+	
+	
 }
