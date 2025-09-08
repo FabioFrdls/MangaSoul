cb05e2d2f578d395a6688bf48934d05bd0d01366
SalvatoreBaiano <salvatorebaiano305@gmail.com>
2025-09-08 13:01:28 +0200

    wrahwhwr

--- Frontend/scripts/MangaCardCreation.js
+++ Frontend/scripts/MangaCardCreation.js
@@ -5,9 +5,9 @@ let cardContainer = document.getElementById("cardContainer"); // html made div
 function createCard(manga) {
     let card = document.createElement("div"); // in all the part like this i create element and give details
     card.classList.add("card");
-    let favorite=document.createElement("span");
+    let favorite = document.createElement("span");
     favorite.classList.add("favorite");
-    favorite.textContent="❤️"
+    favorite.textContent = "♡"
 
     let img = document.createElement("img");
     img.src = manga.image;
@@ -38,7 +38,7 @@ function createCard(manga) {
         openModel(manga);
     });
 
-    card.append(img, title, author, button,favorite); // in this part i m giving the card all the info
+    card.append(img, title, author, button, favorite); // in this part i m giving the card all the info
 
     return card; // i return the card to be appended
 }
@@ -89,44 +89,43 @@ function openModel(manga) {
     let closeButton = document.getElementById("closeButton");
     let mangaReview = document.getElementById("mangaReview");
 
-    modalBody.innerHTML = "";
-    mangaReview.innerHTML = "";
 
-    let img = document.createElement("img");
+
+    let img = document.getElementById("mangaImage");
     img.src = manga.image;
     img.alt = manga.title;
 
-    let title = document.createElement("h5");
+    let title = document.getElementById("mangaTitle");
     title.textContent = manga.title;
 
-    let summary = document.createElement("p");
-    summary.textContent = "Trama: " + manga.summary;
+    let summary = document.getElementById("mangaSummaryBody");
+    summary.textContent = manga.summary;
 
-    let genr = document.createElement("p");
-    genr.textContent = "Genere: " + manga.genres.map(g => g.name).join(", ");
+    let genr = document.getElementById("mangaGenresBody");
+    genr.textContent = manga.genres.map(g => g.name).join(", ");
 
-    let year = document.createElement("p");
-    year.textContent = "Anno d'uscita: " + manga.year;
+    let year = document.getElementById("mangaYearBody");
+    year.textContent =  manga.year;
 
-    let author = document.createElement("p");
-    author.textContent = "Autore: " + manga.author.full_name;
+    let author = document.getElementById("mangaAuthorBody");
+    author.textContent =  manga.author.full_name;
 
-    let editor = document.createElement("p");
-    editor.textContent = "Pubblicazione italiana: " + manga.editor_name;
+    let editor = document.getElementById("mangaEditorBody");
+    editor.textContent =  manga.editor_name;
 
-    let volumes = document.createElement("p");
-    volumes.textContent = "Numeri volumi: " + manga.volumes;
+    let volumes = document.getElementById("mangaVolumesBody");
+    volumes.textContent =  manga.volumes;
 
-    let status = document.createElement("p");
-    status.textContent = "Stato: " + manga.status;
+    let status = document.getElementById("mangaStatusBody");
+    status.textContent = manga.status;
 
-    let score = document.createElement("h3");
+    let score = document.getElementById("mangaScoreBody");
     score.classList.add("score");
     score.textContent = manga.score;
 
-    modalBody.append(img, title, summary, genr, year, author, editor, volumes, status, score);
 
-   // deployReview(manga); // alessio write here the method for the review
+
+    // deployReview(manga); // alessio write here the method for the review
 
     modal.style.display = "flex";
     closeButton.onclick = () => {
